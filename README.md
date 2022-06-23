9f43c0b1ce4a2f0f20dcafbd6cb5bfdc1f490f72
Siddhi-244 <siddhibhanushali1234@gmail.com>
2022-06-23 16:07:27 +0530

    readme added

---
+++ README.md
@@ -0,0 +1,16 @@
+## Functionalities of Expense Tracker
+- Add New Expenses.
+- Filters expenses based on year.
+- Conditional Rendering implemented.
+- Data is taken from the user.
+- Mobile responsive
+
+## Tech Stack
+- React JS
+- HTML
+- CSS
+
+## Demo
+![image 1](images/1.png)
+![image 2](images/2.png)
+![image 3](images/3.png)
--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './ExpenseDate.css';
 function ExpenseDate(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const month = props.date.toLocaleString("en-US", { month: "long" }); // date component 
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
     <div className='expense-date'>
-      <div className='expense-date__month'>{month}</div>
+      <div className='expense-date__month'>{month}</div>  {/* extracted date object into day, month and year */}
       <div className='expense-date__year'>{year}</div>
       <div className='expense-date__day'>{day}</div>
     </div>
